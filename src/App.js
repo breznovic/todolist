f3a5a40b328e2f48a4b4bac07d427c175894668c
Breznovic <breznovic@mail.ru>
2021-09-29 20:48:37 +0300

    Only hooks

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-
+import React from 'react';
 import './App.css';
+import HookCounter from './Components/HookCounter';
 
 function App() {
   return (
-    <div className="App">
-      Codevolution
+    <div className='app'>
+      <HookCounter />
     </div>
   );
 }
