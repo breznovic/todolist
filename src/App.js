053afde760cb2a440fc4262ca08d5ecf073bd10d
Breznovic <breznovic@mail.ru>
2021-10-01 10:12:19 +0300

    Else

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import HookCounter from './Components/HookCounter';
+import HookCounter1 from './Components/HookCounter1';
 import HookCounterTwo from './Components/HookCounterTwo';
 
 function App() {
   return (
     <div className='app'>
       <HookCounterTwo />
+      <HookCounter1 />
     </div>
   );
 }
