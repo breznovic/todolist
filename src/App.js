f2e5dc2c3a3b652b32be216db9676c2c5c785ed5
Breznovic <breznovic@mail.ru>
2021-10-25 14:29:44 +0300

    Count Hook

--- src/App.js
+++ src/App.js
@@ -3,15 +3,20 @@ import './App.css'
 import { FaReact } from 'react-icons/fa'
 import { BiAt, BiAtom } from 'react-icons/bi'
 import { IconContext } from 'react-icons'
+import HookCounter from './Components/HookCounter'
+
 
 
 function App() {
   return (
     <IconContext.Provider value={{ color: 'red', size: '6rem' }}>
       <div className='app'>
+        <div>
         <FaReact />
         <BiAt color='green' />
         <BiAtom />
+        <HookCounter />
+        </div>
       </div>
     </IconContext.Provider>
   )
