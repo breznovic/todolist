5f08886414b6b51432c10862630184b829022922
Breznovic <breznovic@mail.ru>
2021-11-01 10:47:44 +0300

    Changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
 import React from 'react'
-import './App.css'
-import { FaReact } from 'react-icons/fa'
-import { BiAt, BiAtom } from 'react-icons/bi'
-import { IconContext } from 'react-icons'
-import HookCounter from './Components/HookCounter'
-import Counter from './Components/Counter'
+import classes from './App.module.css'
 
 
 
 function App() {
   return (
-    <IconContext.Provider value={{ color: 'red', size: '6rem' }}>
-      <div className='app'>
-        <div>
-        <FaReact />
-        <BiAt color='green' />
-        <BiAtom />
-        <HookCounter />
-        <Counter />
-        </div>
-      </div>
-    </IconContext.Provider>
+    <div>
+
+    </div>
+
   )
 }
 
