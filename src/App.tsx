0818f63cba5def4c2505c39bad41cea2b84bf458
breznovic <slymarkel@gmail.com>
2021-12-25 12:33:54 +0300

    2 lesson

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './App.css'
 import Todolist from "./Todolist";
 import {v1} from "uuid";
@@ -9,17 +9,44 @@ export type TaskType = {
     isDone: boolean
 }
 
+export type FilterType = 'all' | 'active' | 'completed'
+
 function App() {
 
-    let tasks = [
+    let [tasks, setTasks] = useState([
         {id: v1(), title: 'HTML', isDone: true},
         {id: v1(), title: 'CSS', isDone: true},
         {id: v1(), title: 'React', isDone: false},
-    ]
+    ])
+
+    let [filter, setFilter] = useState<FilterType>('all')
+
+    let taskTodo = tasks
+
+    if (filter === 'active') {
+        taskTodo = tasks.filter(t => !t.isDone)
+    }
+    if (filter === "completed") {
+        taskTodo = tasks.filter(t => t.isDone)
+    }
+
+    function newTask (title: string) {
+        let nTask = {id: v1(), title: title, isDone:false}
+        let nwTask = [nTask, ...tasks]
+        setTasks(nwTask)
+
+    }
+
+    function deleteTask (id: string) {
+        let tasksForTodo = tasks.filter(t => t.id !== id)
+        setTasks(tasksForTodo)
+    }
 
     return <div>
         <Todolist title='What to learn'
-                  tasks={tasks}
+                  tasks={taskTodo}
+                  deleteTask={deleteTask}
+                  newtask={newTask}
         />
     </div>
 }
--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,24 +1,35 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './App.css'
 import {TaskType} from "./App";
 
 type PropsType = {
     title: string
     tasks: Array<TaskType>
+    deleteTask: (id: string) => void
+    newTask: (title: string) => void
 }
 
 function Todolist(props: PropsType) {
-    return   <div>
+
+    let [title, setTitle] = useState('')
+
+    const newTask = (e: number) => {props.newTask(title)}
+
+    return <div>
         <h3>{props.title}</h3>
         <div>
-            <input/>
-            <button>+</button>
+            <input value={title} onChange={(e) => {setTitle(e.currentTarget.value)}} />
+            <button onClick={newTask}>+</button>
         </div>
         <div>
             {
-                props.tasks.map(t => <div key={t.id}>
-                    <input type="checkbox" checked={t.isDone}/><span>{t.title}</span>
-                </div>)
+                props.tasks.map(t => {
+
+                    const deleteTask = () => props.deleteTask(t.id)
+
+                    return <div key={t.id}><input type="checkbox" checked={t.isDone}/><span>{t.title}</span>
+                    <button onClick={deleteTask}>x</button>
+                </div>})
             }
         </div>
         <div>
