fc4b6cd3ab32ceba4ee002d23fb01ad6772e2852
breznovic <slymarkel@gmail.com>
2022-01-03 23:14:17 +0300

    Scratch

--- src/App.tsx
+++ src/App.tsx
@@ -8,18 +8,6 @@ export type TaskType = {
     isDone: boolean
 }
 
-type TaskStateType = {
-[key: string]: Array<TaskType>
-}
-
-export type FilterType = 'all' | 'active' | 'completed'
-
-type TodolistType = {
-    id: string
-    title: string
-    filter: FilterType
-}
-
 function App() {
 
     return <div className='app'>
