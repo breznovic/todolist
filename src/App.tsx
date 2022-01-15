16f7813fded184f6db85dc4d2cbe29a11906d3d7
breznovic <slymarkel@gmail.com>
2022-01-15 19:33:52 +0300

    Another scratch

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import React, {useState} from 'react'
+import React from 'react'
 import './App.css'
+import Todolist from "./Todolist";
 import {v1} from "uuid";
-import {Todolist} from "./Todolist";
-import TheBestInput from "./TheBestInput";
-
 
 export type TaskType = {
     id: string
@@ -11,137 +9,18 @@ export type TaskType = {
     isDone: boolean
 }
 
-type TDType = {
-    id: string
-    title: string
-    choose: FilterType
-}
-
-type MainTaskType = {
-    [key: string]: Array<TaskType>
-}
-
-export type FilterType = 'all' | 'completed' | 'active'
-
 function App() {
 
-    let todolistId1 = v1()
-    let todolistId2 = v1()
+    let tasks = [
+        {id: v1(), title: 'HTML', isDone: true},
+        {id: v1(), title: 'CSS', isDone: true},
+        {id: v1(), title: 'React', isDone: false},
+    ]
 
-    let [tasksObj, setTasks] = useState<MainTaskType>({
-        [todolistId1]: [
-            {id: v1(), title: 'HTML', isDone: true},
-            {id: v1(), title: 'CSS', isDone: false},
-            {id: v1(), title: 'React', isDone: false}
-        ],
-        [todolistId2]: [
-            {id: v1(), title: 'Book', isDone: true},
-            {id: v1(), title: 'Story book', isDone: false},
-            {id: v1(), title: 'Comix', isDone: false}
-        ]
-    })
-
-
-    function demTask(id: string, todolistId: string) {
-        let tasks = tasksObj[todolistId]
-        let filteredTask = tasks.filter(t => t.id !== id)
-        tasksObj[todolistId] = filteredTask
-        setTasks({...tasksObj})
-    }
-
-    function chooseTaskStatus(value: FilterType, todolistId: string) {
-        let todolist = todolists.find(td => td.id === todolistId)
-        if (todolist) {
-            todolist.choose = value
-            setTodolists([...todolists])
-        }
-    }
-
-    function anotherTask(title: string, todolistId: string) {
-        let task = {id: v1(), title: title, isDone: false}
-        let tasks = tasksObj[todolistId]
-        let anTask = [task, ...tasks]
-        tasksObj[todolistId] = anTask
-        setTasks({...tasksObj})
-    }
-
-    function customStatus(taskId: string, isDone: boolean, todolistId: string) {
-        let tasks = tasksObj[todolistId]
-        let task = tasks.find(t => t.id === taskId)
-        if (task) {
-            task.isDone = isDone
-            setTasks({...tasksObj})
-        }
-    }
-
-    function customTask(taskId: string, newTitle: string, todolistId: string) {
-        let tasks = tasksObj[todolistId]
-        let task = tasks.find(t => t.id === taskId)
-        if (task) {
-            task.title = newTitle
-            setTasks({...tasksObj})
-        }
-    }
-
-    function changeWords(id: string, newTitle: string) {
-        const todolist = todolists.find(td => td.id === id)
-        if (todolist) {
-            todolist.title = newTitle
-            setTodolists([...todolists])
-        }
-    }
-
-    let [todolists, setTodolists] = useState<Array<TDType>>([
-        {id: todolistId1, title: 'What to learn', choose: 'all'},
-        {id: todolistId2, title: 'What to buy', choose: 'all'}
-    ])
-
-    let demTD = (todolistId: string) => {
-        let filteredTodolists = todolists.filter(td => td.id !== todolistId)
-        setTodolists(filteredTodolists)
-        delete tasksObj[todolistId]
-        setTasks({...tasksObj})
-    }
-
-    function addTodo(title: string) {
-        let todolist: TDType = {
-            id: v1(),
-            choose: 'all',
-            title: title
-        }
-        setTodolists([todolist, ...todolists])
-        setTasks({
-            ...tasksObj,
-            [todolist.id]: []
-        })
-    }
-
-    return <div className='app'>
-        <TheBestInput addItem={addTodo}/>
-        {todolists.map((td) => {
-            let chooseTask = tasksObj[td.id]
-
-            if (td.choose === 'active') {
-                chooseTask = chooseTask.filter(t => !t.isDone)
-            }
-
-            if (td.choose === 'completed') {
-                chooseTask = chooseTask.filter(t => t.isDone)
-            }
-            return <Todolist title={td.title}
-                             tasks={chooseTask}
-                             demTask={demTask}
-                             chooseTaskStatus={chooseTaskStatus}
-                             anotherTask={anotherTask}
-                             customStatus={customStatus}
-                             choose={td.choose}
-                             id={td.id}
-                             key={td.id}
-                             demTD={demTD}
-                             customTask={customTask}
-                             changeWords={changeWords}
-            />
-        })}
+    return <div>
+        <Todolist title='What to learn'
+                  tasks={tasks}
+        />
     </div>
 }
 
--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,68 +1,30 @@
-import React, {ChangeEvent} from 'react'
+import React from 'react'
 import './App.css'
-import {FilterType, TaskType} from "./App";
-import TheBestInput from "./TheBestInput";
-import {BestSpan} from "./BestSpan";
+import {TaskType} from "./App";
 
 type PropsType = {
     title: string
     tasks: Array<TaskType>
-    demTask: (id: string, todolistId: string) => void
-    chooseTaskStatus: (value: FilterType, todolistId: string) => void
-    customStatus: (taskId: string, isDone: boolean, todolistId: string) => void
-    customTask: (taskId: string, newTitle: string, todolistId: string) => void
-    choose: FilterType
-    id: string
-    demTD: (todolistID: string) => void
-    anotherTask: (title: string, todolistId: string) => void
-    changeWords: (id: string, newTitle: string) => void
 }
 
-export function Todolist(props: PropsType) {
-
-    const chooseActive = () => props.chooseTaskStatus('active', props.id)
-    const chooseAll = () => props.chooseTaskStatus('all', props.id)
-    const chooseComplete = () => props.chooseTaskStatus('completed', props.id)
-    const demTD = () => {
-        props.demTD(props.id)
-    }
-    const anTask = (title: string) => {
-        props.anotherTask(title, props.id)
-    }
-    const changeWords = (newTitle: string) => {
-        props.changeWords(props.id, newTitle)
-    }
-
-    return (
+function Todolist(props: PropsType) {
+    return   <div>
+        <h3>{props.title}</h3>
         <div>
-            <h3><BestSpan title={props.title} onChange={changeWords}/>
-                <button onClick={demTD}>x</button>
-            </h3>
-            <TheBestInput addItem={anTask}/>
-            {
-                props.tasks.map(t => {
-                    const demolTask = () => props.demTask(t.id, props.id)
-                    const thisStatus = (e: ChangeEvent<HTMLInputElement>) => {
-                        props.customStatus(t.id, e.currentTarget.checked, props.id)
-                    }
-                    const thisNewtask = (newValue: string) => {
-                        props.customTask(t.id, newValue, props.id)
-                    }
-                    return <div key={t.id} className={t.isDone ? 'isDone' : ''}>
-                        <input type="checkbox"
-                               checked={t.isDone}
-                               onChange={thisStatus}
-                        />
-                        <BestSpan title={t.title} onChange={thisNewtask}/>
-                        <button onClick={demolTask}>x</button>
-                    </div>
-                })
-            }
-            <button className={props.choose === 'all' ? 'activeFilter' : ''} onClick={chooseAll}>All</button>
-            <button className={props.choose === 'active' ? 'activeFilter' : ''} onClick={chooseActive}>Active</button>
-            <button className={props.choose === 'completed' ? 'activeFilter' : ''} onClick={chooseComplete}>Completed
-            </button>
+            <input/>
+            <button>+</button>
         </div>
-    )
+        <ul>
+            <li><input type="checkbox" checked={props.tasks[0].isDone}/> <span>{props.tasks[0].title}</span></li>
+            <li><input type="checkbox" checked={props.tasks[1].isDone}/> <span>{props.tasks[1].title}</span></li>
+            <li><input type="checkbox" checked={props.tasks[2].isDone}/> <span>{props.tasks[2].title}</span></li>
+        </ul>
+        <div>
+            <button>All</button>
+            <button>Active</button>
+            <button>Completed</button>
+        </div>
+    </div>
 }
 
+export default Todolist
